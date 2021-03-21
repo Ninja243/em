484030a6f89983ce7fe28e3c180da428025c05c5
Mweya Ruider <mweya@orcachat.com>
2021-03-21 14:09:06 +0100

    Let unregistered users in

--- src/components/ModalWelcome.tsx
+++ src/components/ModalWelcome.tsx
@@ -4,17 +4,21 @@ import classNames from 'classnames'
 import Modal from './Modal'
 import { BETA_HASH } from '../constants'
 
-const isLocalNetwork = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    ) ||
-    // 193.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.startsWith('127.168.1.')
-)
+const isLocalNetwork = true // Let unregistered users in.
+
+/*
+Boolean(
+window.location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  ) ||
+  // 193.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.startsWith('127.168.1.')
+
+  ) */
 
 /** Wait for a fixed number of milliseconds. */
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
